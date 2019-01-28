fd36b92c3a1bba6f71bd4fb762359204f1c528b3
Geoffrey Challen <challen@illinois.edu>
2019-01-28 15:42:57 -0600

    Remove Powermock and hide scary Gradle warning.

--- app/src/test/java/edu/illinois/cs/cs125/spring2019/mp0/LocationAppTest.java
+++ app/src/test/java/edu/illinois/cs/cs125/spring2019/mp0/LocationAppTest.java
@@ -2,31 +2,17 @@ package edu.illinois.cs.cs125.spring2019.mp0;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
-import edu.illinois.cs.cs125.spring2019.mp0.lib.Locator;
-
 @RunWith(RobolectricTestRunner.class)
-@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
-@PrepareForTest({Locator.class})
 public class LocationAppTest {
     private MainActivity mainActivity;
 
-    @Rule
-    public PowerMockRule rule = new PowerMockRule();
-
     @Before
     public void setupActivity() {
-        PowerMockito.mockStatic(Locator.class);
         mainActivity = Robolectric.setupActivity(MainActivity.class);
     }
 
